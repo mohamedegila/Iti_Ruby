d1104e5d1ac95497fd035257f20edf00758f5c12
mohammedegila <mohammedegila@gmail.com>
2021-05-09 14:26:50 +0200

    Add validations on create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,15 +8,20 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
-    def new
-    end
 
-    def create
-        @article = Article.new(article_params)
-       
-        @article.save
-        redirect_to @article
-      end
+def new
+    @article = Article.new
+  end
+   
+  def create
+    @article = Article.new(article_params)
+   
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
        
       private
         def article_params
