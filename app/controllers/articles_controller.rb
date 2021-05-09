c8501d8e849a1a13bcf2b6cbc51db69da8c28d39
mohammedegila <mohammedegila@gmail.com>
2021-05-09 14:31:29 +0200

    Add update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,24 +9,38 @@ class ArticlesController < ApplicationController
     end
 
 
-def new
-    @article = Article.new
-  end
-   
-  def create
-    @article = Article.new(article_params)
-   
-    if @article.save
-      redirect_to @article
-    else
-      render 'new'
+    def new
+        @article = Article.new
     end
-  end
-       
-      private
-        def article_params
-          params.require(:article).permit(:title, :text)
+   
+    def create
+        @article = Article.new(article_params)
+   
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
         end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+    end
+       
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
       
 
 end
